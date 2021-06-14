f14cf07e5a6245571a686023f09d095767ef6a1a
DKendle <dina.kendle@gmail.com>
2021-06-14 13:08:33 -0400

    commit tree 109707e1a00cf763a398d3fb8ffd0f50c5302086

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (tutorials) => {
+  return tutorials.map(tutorial => tutorial.toUpperCase())
 }
+
